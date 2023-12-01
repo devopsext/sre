196cf5669974bad0f796b2f3a6eba08432ae3537
Sergei Tomilov <tsv.titan@gmail.com>
2023-12-01 16:37:49 +0200

    Added Gauge to metrics

--- common/metrics.go
+++ common/metrics.go
@@ -5,6 +5,11 @@ type MetricsCounter struct {
 	metrics  *Metrics
 }
 
+type MetricsGauge struct {
+	gauges  map[Meter]Gauge
+	metrics *Metrics
+}
+
 type Metrics struct {
 	meters []Meter
 }
@@ -32,6 +37,31 @@ func (ms *Metrics) Counter(name, description string, labels []string, prefixes .
 		}
 	}
 	return &counter
+}
+
+func (msg *MetricsGauge) Set(value float64, values ...string) Gauge {
+
+	for _, m := range msg.gauges {
+		m.Set(value, values...)
+	}
+	return msg
+}
+
+func (ms *Metrics) Gauge(name, description string, labels []string, prefixes ...string) Gauge {
+
+	gauge := MetricsGauge{
+		metrics: ms,
+		gauges:  make(map[Meter]Gauge),
+	}
+
+	for _, m := range ms.meters {
+
+		g := m.Gauge(name, description, labels, prefixes...)
+		if g != nil {
+			gauge.gauges[m] = g
+		}
+	}
+	return &gauge
 }
 
 func (ms *Metrics) Stop() {
