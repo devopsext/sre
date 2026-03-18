26ac0701d2fa32efc919b4940d17a35e616b801f
dshaparenko <llapmaw@gmail.com>
2026-03-18 10:51:48 +0200

    version go up plus signal fix in root

--- cmd/root.go
+++ cmd/root.go
@@ -162,7 +162,7 @@ var grafanaEventerOptions = provider.GrafanaEventerOptions{
 
 func interceptSyscall() {
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		<-c
